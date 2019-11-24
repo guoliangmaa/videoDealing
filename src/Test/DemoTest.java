ee3b7e290e1a2889697c520a9b144d95425a18cd
ysuMgl <772399887@qq.com>
2019-11-24 23:37:46 +0800

    我的第一次提交

---
+++ src/Test/DemoTest.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class DemoTest {
+
+    public void test1(){
+        System.out.println("laalalaaaa");
+    }
+
+    public static void main(String[] args) {
+        DemoTest demoTest = new DemoTest();
+        demoTest.test1();
+    }
+}
